0bf800ebe72c35c16f50a99274de08f1aa2e2f4e
Joachimzeelmaekers <joa.zeelmaekers@hotmail.com>
2020-11-11 19:58:46 +0100

    refactor to a card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,42 @@
+import React, {useState} from 'react';
+import Rating from './Rating';
+
+function Card({show}) {
+  const [descriptionOpened, setDescriptionOpened] = useState(false);
+  let description = descriptionOpened
+    ? show.description
+    : show.description.slice(0, 100) + '...';
+  return (
+    <div className="image-container w-1/4 max-w-sm p-2">
+      <div className="container rounded overflow-hidden shadow-lg hover:shadow-2xl cursor-pointer pt-1">
+        <img className="w-full image" src={show.image} alt={show.name} />
+        <div className="px-6 py-4">
+          <div className="font-bold text-l mb-2">{show.name}</div>
+          <p
+            className="text-gray-700 text-base"
+            dangerouslySetInnerHTML={{__html: description}}
+          ></p>
+          <button
+            className="text-sm text-blue-500 hover:text-blue-800"
+            onClick={() => setDescriptionOpened((prevState) => !prevState)}
+          >
+            {descriptionOpened ? 'Toggle description' : 'Show full description'}
+          </button>
+          <Rating showId={show.id} rating={show.rating} />
+        </div>
+        <div className="px-6 pt-4 pb-2">
+          {show.genres.map((genre) => (
+            <span
+              key={`${show.name}-${genre}`}
+              className="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 mr-1 mb-1"
+            >
+              {genre}
+            </span>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
--- src/pages/showsOverview/ShowsOverview.jsx
+++ src/pages/showsOverview/ShowsOverview.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import Rating from '../../components/Rating';
+import Card from '../../components/Card';
 import Search from '../../components/Search';
 
 // const API_URL = "https://api.tvmaze.com";
@@ -39,46 +39,10 @@ function ShowsOverview() {
           setSearchValue={setSearchValue}
         />
       </header>
-      <div className="antialiased font-sans flex flex-wrap mb-4 p-6 ">
-        {shows.map((show) => {
-          const shortSummary = show.description.slice(0, 100) + '...';
-          return (
-            <div
-              className="image-container w-1/4 max-w-sm p-2"
-              key={`${show.name}-${show.id}`}
-            >
-              <div className="container rounded overflow-hidden shadow-lg hover:shadow-2xl cursor-pointer pt-1">
-                <img
-                  className="w-full image"
-                  src={show.image}
-                  alt={show.name}
-                />
-                <div className="px-6 py-4">
-                  <div className="font-bold text-l mb-2">{show.name}</div>
-                  <p
-                    className="text-gray-700 text-base"
-                    dangerouslySetInnerHTML={{__html: shortSummary}}
-                  ></p>
-                  <button className="my-6 text-blue-500 hover:text-blue-800">
-                    Show full description
-                  </button>
-                  <Rating showId={show.id} rating={show.rating} />
-                </div>
-
-                <div className="px-6 pt-4 pb-2">
-                  {show.genres.map((genre) => (
-                    <span
-                      key={`${show.name}-${genre}`}
-                      className="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 mr-1 mb-1"
-                    >
-                      {genre}
-                    </span>
-                  ))}
-                </div>
-              </div>
-            </div>
-          );
-        })}
+      <div className="antialiased font-sans flex flex-wrap mb-4 p-6">
+        {shows.map((show) => (
+          <Card show={show} key={`${show.name}-${show.id}`} />
+        ))}
       </div>
     </div>
   );
